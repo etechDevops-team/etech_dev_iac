be54119948685d5bb85d60cb4c9a5bcd9084159b
chewy37 <immakosgei@yahoo.com>
2025-06-06 17:27:44 -0500

    Remove security group data source to fix multiple matches error

--- minimal.tf
+++ minimal.tf
@@ -10,17 +10,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Use existing security group instead of creating one
-data "aws_security_group" "existing_sg" {
-  name = "default"
-}
-
 # Use existing IAM role
 data "aws_iam_role" "existing_role" {
   name = "WordpressRole"
 }
 
-# Use existing instance profile or create if it doesn't exist
+# Create only the instance profile
 resource "aws_iam_instance_profile" "profile" {
   name = "WordpressProfile"
   role = data.aws_iam_role.existing_role.name
